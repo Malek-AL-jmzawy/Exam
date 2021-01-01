a9decab19b0f11f363ffa2bfd47f9cb15e80c037
malek88888 <jmzomalek@gmail.com>
2021-01-01 22:56:09 +0200

    part4 still under work

--- part_4/part_4.js
+++ part_4/part_4.js
@@ -11,8 +11,19 @@ return 'NO' if the parentheses in the input string are not balanced,.
 
 console.log('PART 4');
 
-const isABalancedString = () => {
+const isABalancedString = (str) => {
   // WRITE YOUR CODE BELOW THIS LINE
+  const arr =str.split("")
+  var sqr=0
+  var para=0
+  var carly=0
+  for (let i = 0; i < arr.length; i++) {  
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j]==="]") {
+        
+      }
+    }  
+  }
 };
 
 /*
