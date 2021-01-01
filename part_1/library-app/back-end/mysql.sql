ccb878649b15008ac69e64c88d7d36e8bbaa20e1
malek88888 <jmzomalek@gmail.com>
2021-01-01 22:12:28 +0200

    Q1 ,2,3,4,5 SOLVED

---
+++ part_1/library-app/back-end/mysql.sql
@@ -0,0 +1,9 @@
+use test;
+create table books (
+title varchar(255) ,
+ author varchar(255),
+ pages int,
+ publisher varchar(255)
+, published_at varchar(255),
+primary key (title)
+);
--- part_1/library-app/back-end/server.js
+++ part_1/library-app/back-end/server.js
@@ -1,7 +1,28 @@
 const express = require("express");
-
+const connection=require("./db")
 const app = express();
+// app.use()
+app.get("/books",(req,res)=>{
+  const query=`SELECT * FROM books`
+  connection.query(query,(err, results)=>{
+    console.log(results);
+  })
+})
 
+app.get("/books/:book_id",(req,res)=>{
+  const query=`SELECT * FROM books WHERE book_id =?`
+  const data =req.params.book_id
+  connection.query(query,data,(err, results)=>{
+    console.log(results);
+  })
+})
+app.delete("/books/:book_id",(req,res)=>{
+  const query=`DELETE FROM books WHERE book_id =?`
+  const data =req.params.book_id
+  connection.query(query,data,(err, results)=>{
+    console.log(results);
+  })
+})
 const port = 3000;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
